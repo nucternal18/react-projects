5fc214af6ee8e86414e80ac8fc332381d7958dc3
A Oladipupo-Usoro <aolausoro@gmail.com>
2020-11-20 23:56:40 +0000

    worked on poject 12-sidebar-modal using context API

--- 12-sidebar-modal/setup/src/context.js
+++ 12-sidebar-modal/setup/src/context.js
@@ -1,1 +1,40 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, useContext, createContext } from 'react';
+
+const AppContext = createContext();
+
+const AppProvider = ({ children }) => {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const openSidebar = () => {
+        setIsSidebarOpen(true)
+    }
+
+    const closeSidebar = () => {
+        setIsSidebarOpen(false)
+    }
+    const openModal = () => {
+        setIsModalOpen(true)
+    }
+
+    const closeModal = () => {
+        setIsModalOpen(false)
+    }
+    return <AppContext.Provider value={{
+        isModalOpen,
+        isSidebarOpen,
+        openModal,
+        closeModal,
+        openSidebar,
+        closeSidebar
+    }}>
+        {children}
+    </AppContext.Provider>;
+};
+
+// Custom Hook
+export const useGlobalContext = () => {
+    return useContext(AppContext)
+}
+
+export { AppContext, AppProvider };
