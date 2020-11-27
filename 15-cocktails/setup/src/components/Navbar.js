5c34dc74b18870cbf43d8b85e38b2b579d4863be
A Oladipupo-Usoro <aolausoro@gmail.com>
2020-11-27 23:29:30 +0000

    worked on poject 15-cocktails. setup about, error and home pages. created fetchDrinks function in context to ftech cocktail data

--- 15-cocktails/setup/src/components/Navbar.js
+++ 15-cocktails/setup/src/components/Navbar.js
@@ -4,9 +4,17 @@ import logo from '../logo.svg'
 
 const Navbar = () => {
   return (
-    <div>
-      <h2>navbar component</h2>
-    </div>
+    <nav className="navbar">
+      <div className="nav-center">
+        <Link to="/">
+            <img src={logo} alt="cocktail db logo" className="logo"/>
+        </Link>
+      </div>
+      <ul className="nav-links">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </ul>
+    </nav>
   )
 }
 
--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className='section about-section'>
+      <h1 className='section-title'>About us</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero incidunt
+        iure nisi placeat autem qui repudiandae adipisci nam ipsum. Repudiandae
+        mollitia illum expedita, delectus eveniet et dolor modi, earum soluta
+        consequuntur fugit! Veniam sequi necessitatibus voluptatibus praesentium
+        ex libero officia?
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
